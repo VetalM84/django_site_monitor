70f48b63c126578595d24a37622cefbba83a3f38
Vitalii Mytenko <vetalmit@gmail.com>
2023-05-05 21:39:11 +0300

    Register admin

--- apps/monitor/admin.py
+++ apps/monitor/admin.py
@@ -1,3 +1,33 @@
+"""Backend admin page."""
+
 from django.contrib import admin
 
-# Register your models here.
+from apps.monitor.models import Project, ProjectModule
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    """Project model views on backend."""
+
+    list_display = (
+        "id",
+        "name",
+        "check_interval",
+        "is_active",
+        "last_run",
+        "date_created",
+    )
+    list_display_links = ("id", "name")
+    # TODO: add user field
+    ordering = ("-is_active", "name")
+    search_fields = ("name",)
+
+
+@admin.register(ProjectModule)
+class ProjectModuleAdmin(admin.ModelAdmin):
+    """ProjectModule model views on backend."""
+
+    list_display = ("id", "project", "is_active", "url", "pagination", "css_selector")
+    list_display_links = ("id", "project")
+    ordering = ("is_active", "project")
+    search_fields = ("url", "css_selector")
